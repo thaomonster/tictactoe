21b232fbcf15e6efc12aa4b8ce5f1f30d8d733d8
Thao Ma <MaThao@gmail.com>
2020-11-06 11:59:44 -0700

    Add img to board when player clicks on it

--- game.js
+++ game.js
@@ -2,6 +2,6 @@ class Game {
   constructor() {
     this.playerOne = new Player('harry-potter');
     this.playerTwo = new Player('voldermort');
-    this.data = ['', '', '', '', '', '', '', '', '',];
+    this.boardData = ['', '', '', '', '', '', '', '', '',];
   };
 };
