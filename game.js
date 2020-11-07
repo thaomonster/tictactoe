45745ecb326b2c4bc635d827b332028bd5c61efb
Thao Ma <MaThao@gmail.com>
2020-11-07 13:06:11 -0700

    Track player turn and add data to board

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
 class Game {
   constructor() {
-    this.playerOne = new Player('p1', 'harry-potter', true);
-    this.playerTwo = new Player('p2', 'voldermort', false);
+    this.playerOne = new Player('p1', 'harrypotter.jpg', true);
+    this.playerTwo = new Player('p2', 'voldemort.png', false);
     this.board = ['', '', '', '', '', '', '', '', ''];
   };
 
   switchPlayer() {
-    if (this.playerOne.turn === true) {
-      this.playerTwo.turn === false;
-    } else if (this.playerTwo.turn === true) {
-      this.playerOne.turn === false;
+    if (this.playerOne.turn) {
+      this.playerOne.turn = false;
+      this.playerTwo.turn = true;
+    } else {
+      this.playerOne.turn = true;
+      this.playerTwo.turn = false;
     };
   };
 };
--- main.js
+++ main.js
@@ -8,11 +8,15 @@ gameBoard.addEventListener('click', playerClick);
 function playerClick(event) {
   for (var i = 0; i < box.length; i++) {
     if (box[i].id === event.target.id) {
-      // box[i].id.classList.add(currentPlayer);
-      box[i].innerHTML = `<img src="./assets/harrypotter.jpg" class="box-img">`;
-      //game.board[i] = currentPlayer
+      if (game.playerOne.turn === true) {
+        game.board[i] = game.playerOne.id;
+        box[i].innerHTML = `<img src="./assets/${game.playerOne.token}" class="box-img">`;
+      } else {
+        game.board[i] = game.playerTwo.id;
+        box[i].innerHTML = `<img src="./assets/${game.playerTwo.token}" class="box-img">`;
+      }
     };
   };
+  game.switchPlayer(); 
+  console.log(game.board)
 };
-
-
